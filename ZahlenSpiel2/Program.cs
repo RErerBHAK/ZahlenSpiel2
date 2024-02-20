fc58656dcfde406d6150e67ca52907f0ebfcc670
RErerBHAK <119053722+RErerBHAK@users.noreply.github.com>
2024-02-20 13:48:36 +0100

    Int mit Schätzung und Willkommenstext

--- ZahlenSpiel2/Program.cs
+++ ZahlenSpiel2/Program.cs
@@ -5,5 +5,11 @@
         Random random = new Random();
         int secretNumber = random.Next(1, 101); // Zufällige Zahl zwischen 1 und 100
         int attempts = 0;
+
+        Console.WriteLine("Willkommen zum Zahlenratespiel!");
+ 
+
+        int guess = 0;
+        do
     }
 }
